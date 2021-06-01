f6b92a5df8a58023b31d69e2b8866ef364f2b12e
darwindom <joker.tlv12@gmail.com>
2021-06-01 05:13:49 +0300

    Basic Start

---
+++ script.js
@@ -0,0 +1,28 @@
+const morning = document.querySelector('.validate-2');
+const evening = document.querySelector('.validate-3');
+const night = document.querySelector('.validate-4');
+const shab = document.querySelector('.validate-5');
+const btn = document.querySelector('.btn-large');
+const out = document.querySelector('.out');
+const check = document.querySelector('.check')
+
+
+btn.onclick = () => {
+let sum = (a, b, c, d) => a + b + c + d;
+let result = sum((+morning.value * 256), (+evening.value * 304), (+night.value * 352), (+shab.value * 384))
+out.innerHTML = result;
+
+check.onchange = () => {
+    if(check.checked){
+        out.innerHTML = +result + (+result * 6 / 100);
+    }
+   
+}
+}
+
+
+
+    
+
+
+
